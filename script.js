5807eb11b3beab17a99fd2a710054df0838f9d15
Rajjoooo <rajatvarshneywork@gmail.com>
2024-07-14 17:06:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+const gameBoard = document.getElementById('game-board');
+const cardsArray = [
+    'A', 'A', 'B', 'B', 'C', 'C', 'D', 'D',
+    'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H'
+];
+let firstCard = null;
+let secondCard = null;
+let lockBoard = false;
+let matches = 0;
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+shuffle(cardsArray);
+
+function createBoard() {
+    cardsArray.forEach(card => {
+        const cardElement = document.createElement('div');
+        cardElement.classList.add('card');
+        cardElement.dataset.value = card;
+        cardElement.addEventListener('click', flipCard);
+        gameBoard.appendChild(cardElement);
+    });
+}
+
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
+    this.classList.add('flipped');
+    this.innerText = this.dataset.value;
+
+    if (!firstCard) {
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    checkForMatch();
+}
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.value === secondCard.dataset.value;
+
+    isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    
+    resetBoard();
+    matches++;
+    if (matches === cardsArray.length / 2) {
+        setTimeout(() => alert('You won!'), 500);
+    }
+}
+
+function unflipCards() {
+    lockBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flipped');
+        secondCard.classList.remove('flipped');
+        firstCard.innerText = '';
+        secondCard.innerText = '';
+        resetBoard();
+    }, 1000);
+}
+
+function resetBoard() {
+    [firstCard, secondCard, lockBoard] = [null, null, false];
+}
+
+createBoard();
